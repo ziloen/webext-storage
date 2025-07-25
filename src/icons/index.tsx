0af4a9443754deec3e807a545a93593c392f9fd6
N <34268416+ziloen@users.noreply.github.com>
2025-07-25 19:37:52 +0800

    collapse similar id keys

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -1,4 +1,4 @@
-import { SVGProps } from 'react'
+import type { SVGProps } from 'react'
 
 export function CodiconCollapseAll(props: SVGProps<SVGSVGElement>) {
   return (
@@ -58,6 +58,24 @@ export function CodiconChevronDown(props: SVGProps<SVGSVGElement>) {
         d="m7.976 10.072l4.357-4.357l.62.618L8.284 11h-.618L3 6.333l.619-.618z"
         clipRule="evenodd"
       ></path>
+    </svg>
+  )
+}
+
+export function CarbonChevronRight(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 32 32"
+      {...props}
+    >
+      {/* Icon from Carbon by IBM - undefined */}
+      <path
+        fill="currentColor"
+        d="M22 16L12 26l-1.4-1.4l8.6-8.6l-8.6-8.6L12 6z"
+      />
     </svg>
   )
 }
--- src/pages/devtools-panel/App.tsx
+++ src/pages/devtools-panel/App.tsx
@@ -4,7 +4,7 @@ import { noop } from 'es-toolkit'
 import { useEffect, useMemo, useRef, useState } from 'react'
 import { createContext, useContextSelector } from 'use-context-selector'
 import { useGetState, useLatest, useMemoizedFn } from '~/hooks'
-import { CodiconCollapseAll } from '~/icons'
+import { CarbonChevronRight, CodiconCollapseAll } from '~/icons'
 import { evalFn, getProxyStorage } from '~/utils'
 
 // Highlight search value
@@ -23,7 +23,7 @@ function useUnmountSignal() {
 export function App() {
   const [targetState, setTargetState, getTargetState] = useGetState<Record<
     string,
-    unknown
+    { key: string; normalizedKey: string | null; value: unknown }
   > | null>(null)
   const [highlightKeys, setHighlightKeys] = useState(
     new Map<
@@ -35,8 +35,10 @@ export function App() {
     >(),
   )
   const latestHighlightKeys = useLatest(highlightKeys)
-
   const unmountSignal = useUnmountSignal()
+  const [expandedKeys, setExpandedKeys, getExpandedKeys] = useGetState(
+    () => new Set<string>(),
+  )
 
   const [bytesInUse, setBytesInUse] = useState(0)
 
@@ -77,7 +79,11 @@ export function App() {
 
         for (const [key, change] of Object.entries(changes)) {
           if (Object.hasOwn(change, 'newValue')) {
-            nextState[key] = change.newValue
+            nextState[key] = {
+              key,
+              normalizedKey: getNormalizedIdKey(key),
+              value: change.newValue,
+            }
             const hasOld = Object.hasOwn(change, 'oldValue')
             setHighlightKeys((pre) => {
               if (pre.has(key)) {
@@ -127,6 +133,75 @@ export function App() {
     )
   })
 
+  const onToggleExpand = useMemoizedFn((normalizedKey: string | null) => {
+    if (normalizedKey === null) return
+
+    const currentExpandedKeys = getExpandedKeys()
+    const nextExpandedKeys = new Set(currentExpandedKeys)
+
+    if (nextExpandedKeys.has(normalizedKey)) {
+      nextExpandedKeys.delete(normalizedKey)
+    } else {
+      nextExpandedKeys.add(normalizedKey)
+    }
+
+    setExpandedKeys(nextExpandedKeys)
+  })
+
+  const keysReactNode = useMemo(() => {
+    if (!targetState) return null
+
+    const result = []
+
+    const seenIdKeys = new Set<string>()
+    for (const [key, value] of Object.entries(targetState)) {
+      if (value.normalizedKey) {
+        const expanded = expandedKeys.has(value.normalizedKey)
+
+        if (!seenIdKeys.has(value.normalizedKey)) {
+          seenIdKeys.add(value.normalizedKey)
+          result.push(
+            <KeyDisplay
+              key={value.key}
+              property={value.key}
+              normalizedKey={value.normalizedKey}
+              onToggleExpand={onToggleExpand}
+              value={value.value}
+              expanded={expanded}
+              indent={0}
+            />,
+          )
+        } else if (expanded) {
+          result.push(
+            <KeyDisplay
+              key={value.key}
+              property={value.key}
+              value={value.value}
+              expanded={undefined}
+              indent={1}
+              normalizedKey={value.normalizedKey}
+              onToggleExpand={onToggleExpand}
+            />,
+          )
+        }
+      } else {
+        result.push(
+          <KeyDisplay
+            key={value.key}
+            property={value.key}
+            value={value.value}
+            expanded={undefined}
+            indent={0}
+            normalizedKey={value.normalizedKey}
+            onToggleExpand={onToggleExpand}
+          />,
+        )
+      }
+    }
+
+    return result
+  }, [targetState, expandedKeys])
+
   return (
     <CtxProvider modifiedKeys={highlightKeys}>
       <div className="bg-main-background text-foreground flex size-full flex-col font-sans text-[14px]">
@@ -158,10 +233,7 @@ export function App() {
             'webkit-scrollbar:size-[10px] webkit-scrollbar-thumb:bg-scrollbar-slider_background hover:webkit-scrollbar-thumb:bg-scrollbar-slider_hover-background active:webkit-scrollbar-thumb:bg-scrollbar-slider_active-background webkit-scrollbar-button:hidden',
           )}
         >
-          {targetState &&
-            Object.entries(targetState).map(([key, value]) => (
-              <KeyDisplay key={key} property={key} value={value} />
-            ))}
+          {keysReactNode}
         </div>
       </div>
     </CtxProvider>
@@ -181,10 +253,22 @@ function formatBytes(bytes: number) {
   return `${bytes.toFixed(2)}${labels[n] ?? 'B'}`
 }
 
-function KeyDisplay({ property, value }: { property: string; value: unknown }) {
-  const searchValue = useContextSelector(Ctx, (ctx) =>
-    ctx.searchValue.toLowerCase(),
-  )
+function KeyDisplay({
+  property,
+  value,
+  expanded,
+  normalizedKey,
+  indent,
+  onToggleExpand,
+}: {
+  property: string
+  normalizedKey: string | null
+  value: unknown
+  expanded: boolean | undefined
+  indent: number
+  onToggleExpand: (property: string) => void
+}) {
+  const searchValue = useContextSelector(Ctx, (ctx) => ctx.searchValue)
 
   const excludeArr = useContextSelector(Ctx, (ctx) => ctx.excludeArr)
 
@@ -221,28 +305,41 @@ function KeyDisplay({ property, value }: { property: string; value: unknown }) {
 
   return (
     <div
-      className="group flex h-[22px] justify-between px-[12px] leading-[22px] duration-1000 data-status:duration-0 data-[status=modified]:bg-white/5"
+      className={clsx(
+        'group flex h-[22px] leading-[22px] hover:bg-white/5',
+        expanded && 'sticky top-0',
+      )}
       data-status={status}
-      style={{
-        transitionProperty:
-          'color, background-color, border-color, text-decoration-color, fill, stroke',
-        transitionTimingFunction: 'cubic-bezier(0.4, 0, 0.2, 1)',
-      }}
     >
       <div
         className={clsx(
+          'flex w-full items-center pe-3',
           'duration-1000',
           'group-data-[status=added]:text-added-foreground group-data-[status=added]:duration-0',
           'group-data-[status=deleted]:text-deleted-foreground group-data-[status=deleted]:duration-0',
           'group-data-[status=ignored]:text-ignored-foreground',
-          'group-data-[status=modified]:text-modified-foreground group-data-[status=modified]:duration-0',
+          'group-data-[status=modified]:text-modified-foreground group-data-[status=modified]:bg-white/5 group-data-[status=modified]:duration-0',
         )}
         style={{
           transitionProperty:
             'color, background-color, border-color, text-decoration-color, fill, stroke',
           transitionTimingFunction: 'cubic-bezier(0.4, 0, 0.2, 1)',
+          paddingLeft: `${indent * 16}px`,
         }}
       >
+        {expanded === undefined ? (
+          <div className="mr-1 size-[16px]"></div>
+        ) : (
+          <CarbonChevronRight
+            width={16}
+            height={16}
+            className={clsx('mr-1 cursor-pointer', expanded && 'rotate-90')}
+            onClick={() => {
+              onToggleExpand(normalizedKey!)
+            }}
+          />
+        )}
+
         {property}
       </div>
       {/* <div>{value}</div> */}
@@ -323,7 +420,7 @@ function SearchInput() {
         type="text"
         placeholder="key & value"
         value={searchValue}
-        onChange={(e) => setSearchValue(e.currentTarget.value)}
+        onChange={(e) => setSearchValue(e.currentTarget.value.toLowerCase())}
       />
     </label>
   )
@@ -348,15 +445,58 @@ function SearchExclude() {
   )
 }
 
-function sortObject<T extends Record<string, unknown>>(obj: T): T {
+function sortObject<T extends Record<string, unknown>>(obj: T) {
   // eslint-disable-next-line @typescript-eslint/unbound-method
   const keys = Object.keys(obj).sort(new Intl.Collator().compare)
 
-  const result = {} as T
+  const result = {} as Record<
+    string,
+    { key: string; normalizedKey: string | null; value: unknown }
+  >
 
   for (const key of keys) {
-    result[key] = obj[key]
+    const normalizedKey = getNormalizedIdKey(key)
+    result[key] = {
+      key,
+      normalizedKey: normalizedKey ?? null,
+      value: obj[key],
+    }
   }
 
   return result
+}
+
+const regexPatterns = [
+  /[a-f\d]{24}/,
+  /[\da-f]{8}-[\da-f]{4}-4[\da-f]{3}-[89ab][\da-f]{3}-[\da-f]{12}/,
+]
+
+/**
+ * 如果带有 mongodb 的 ObjectId，或者是 uuid v4 的字符，返回相同的 key
+ * @example
+ * ```ts
+ * const key1 = getNormalizedIdKey('key:60c72b2f9b1e8c001c8e4d3a')
+ * //    ^ 'key:[id]'
+ * const key2 = getNormalizedIdKey('key:123e4567-e89b-12d3-a456-426614174000')
+ * //    ^ 'key:[id]'
+ * const key3 = getNormalizedIdKey('key:normal_value')
+ * //    ^ null
+ * ```
+ */
+function getNormalizedIdKey(key: string): string | null {
+  let matched = false
+
+  const replacer = () => {
+    matched = true
+    return '[id]'
+  }
+
+  for (const pattern of regexPatterns) {
+    const newKey = key.replace(pattern, replacer)
+    if (matched) {
+      return newKey
+    }
+  }
+
+  return null
 }
